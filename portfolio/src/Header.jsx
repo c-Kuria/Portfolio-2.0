3416fd8c44e5a649ce499bc1dd7219c8f1c61dcf
c-Kuria <briankuria1812@gmail.com>
2025-01-15 17:06:07 +0300

    installed packages

---
+++ portfolio/src/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+    return(
+        <header>
+            <h1>Nice</h1>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
